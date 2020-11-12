b1131c4b61d485d011031ada712175a3b767d793
wuyudi <wuyudi119@163.com>
2020-11-13 02:01:45 +0800

    fix config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,16 +1,17 @@
 module.exports = {
-  title: "MMA",
-  description: "Dl",
+  title: "Mathematica 激活指南",
+  description: "Mathematica 的安装、激活、入门、常见问题",
   themeConfig: {
     collapsable: true,
     sidebar: "auto",
     sidebarDepth: 2,
+    editLinks: true,
     sidebar: [
       {
         title: "content",
         collapsable: true,
         children: [
-          ["/content/dl.md", "dl"],
+          ["/content/dl.md", "下载"],
           ["/content/active.md", "激活"],
           ["/content/IntoDoor.md", "入门"],
           ["/content/QA.md", "Q&A"],
